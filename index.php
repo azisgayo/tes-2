39c4b8c646d4184d10bdf6a9563186d192aaedbc
azisgayo <azis.abdul26@gmail.com>
2023-11-04 23:11:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hallo Cantik!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique inventore voluptates velit, nobis praesentium accusantium commodi voluptate labore eum quaerat saepe nihil minus exercitationem? Tempore eum facere quasi voluptatum ut.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
