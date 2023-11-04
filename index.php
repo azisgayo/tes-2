1b46e60614f64cfe2a3919572a3750104bc7a75f
azisgayo <azis.abdul26@gmail.com>
2023-11-04 22:51:54 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote </title>
+</head>
+<body>
+    <h1>Hallo Cantik!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique inventore voluptates velit, nobis praesentium accusantium commodi voluptate labore eum quaerat saepe nihil minus exercitationem? Tempore eum facere quasi voluptatum ut.</p>
+</body>
+</html>
